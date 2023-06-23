767237157dac571e7507bc6c3c5756e7c7a5c034
ivanmuinos <ivan.muinos@directa24.com>
2023-06-23 00:16:15 -0300

    Add axios & connect menu funcionality with strapi

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,35 @@
-import Head from 'next/head'
-import React from 'react'
-import theme from '../components/theme'
-import Header from '../components/Header'
-import { CssBaseline } from '@material-ui/core'
-import { makeStyles, MuiThemeProvider } from '@material-ui/core/styles'
-import PWA from 'react-storefront/PWA'
-import NavBar from '../components/NavBar'
-import reportError from '../components/reportError'
-import useJssStyles from 'react-storefront/hooks/useJssStyles'
-import SessionProvider from 'react-storefront/session/SessionProvider'
-import useAppStore from 'react-storefront/hooks/useAppStore'
+import Head from 'next/head';
+import React from 'react';
+import theme from '../components/theme';
+import Header from '../components/Header';
+import { CssBaseline } from '@material-ui/core';
+import { makeStyles, MuiThemeProvider } from '@material-ui/core/styles';
+import PWA from 'react-storefront/PWA';
+import NavBar from '../components/NavBar';
+import reportError from '../components/reportError';
+import useJssStyles from 'react-storefront/hooks/useJssStyles';
+import SessionProvider from 'react-storefront/session/SessionProvider';
+import useAppStore from 'react-storefront/hooks/useAppStore';
 
-const styles = theme => ({
+const styles = (theme) => ({
   main: {
     paddingTop: 3,
   },
-})
+});
 
-const useStyles = makeStyles(styles)
+const useStyles = makeStyles(styles);
 
 export default function MyApp({ Component, pageProps }) {
-  useJssStyles()
-  const classes = useStyles()
-  const [appData] = useAppStore(pageProps || {})
-
+  useJssStyles();
+  const classes = useStyles();
+  const [appData] = useAppStore(pageProps || {});
   return (
     <PWA errorReporter={reportError}>
-      <Head>
-        {/* <meta
+      <Head>{/* <meta
           key="viewport"
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-        /> */}
-      </Head>
+        /> */}</Head>
       <SessionProvider url="/api/session">
         <MuiThemeProvider theme={theme}>
           <CssBaseline />
@@ -44,15 +41,15 @@ export default function MyApp({ Component, pageProps }) {
         </MuiThemeProvider>
       </SessionProvider>
     </PWA>
-  )
+  );
 }
 
 MyApp.getInitialProps = async function({ Component, ctx }) {
-  let pageProps = {}
+  let pageProps = {};
 
   if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx)
+    pageProps = await Component.getInitialProps(ctx);
   }
 
-  return { pageProps }
-}
+  return { pageProps };
+};
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,54 @@
-import { home } from 'react-storefront-connector'
+import { home } from 'react-storefront-connector';
+import axios from 'axios';
 
 export default async function(req, res) {
-  const response = await home(req, res)
-  console.log(response.appData)
-  res.json(response)
+  const response = await home(req, res);
+  let obj_falopa = await getAppData();
+  let obj_falopa_convert = convertObject(obj_falopa);
+  res.json(mergeObjects(response, obj_falopa_convert));
 }
+
+function convertObject(inputObj) {
+  const data = inputObj.data;
+  const header = data[0].attributes.header;
+  const footer = data[0].attributes.footer;
+  const itemsData = data[0].attributes.items.data;
+
+  const items = itemsData.map((item) => {
+    return {
+      text: item.attributes.text,
+      items: [],
+    };
+  });
+
+  return {
+    items,
+    header,
+    footer,
+  };
+}
+
+function mergeObjects(obj1, obj2) {
+  const mergedObj = {
+    pageData: {
+      ...obj1.pageData,
+      slots: {
+        ...obj1.pageData.slots,
+        ...obj2,
+      },
+    },
+    appData: {
+      ...obj1.appData,
+      menu: {
+        ...obj2,
+      },
+    },
+  };
+
+  return mergedObj;
+}
+
+const getAppData = async () => {
+  const response = await axios.get('http://localhost:1337/api/menus?populate=*');
+  return response.data;
+};
